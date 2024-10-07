a3f5dbd0c43776aa463134664458283b370aed1d
Максим Сидоров <TW1@kvantas.ent>
2024-10-07 14:31:12 +0300

    work with database

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Hello":"W"}
+tasks = []
+
+class StaskAdd(BaseModel):
+    name: str
+    description: Optional[str] | None
+
+class STask(BaseModel):
+    id: int
+
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = STask(name = "Запиши это видео")
+#     return {"data": task}
+
+@app.post("/tasks")
+async def add_task(
+    task: Annotated[StaskAdd, Depends()],
+):
+    tasks.append(tasks)
+    return {"ok": True}
+
